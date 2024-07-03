046e19ceb8f38133accb5fe7a4b8958691ded578
MoatazBadawy <moataz.egy101@gmail.com>
2024-07-03 16:28:24 +0300

    '

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll('a');
     
     links.forEach(link => {
-        link.addEventListener('click', function() {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
             alert('You are now being redirected to ' + this.href);
+            window.location.href = this.href;
         });
     });
 });
